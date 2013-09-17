afa5bf1243bbad6038be5277ccf094fbfea5f2f6
Kevin Ulrich Moschallski <kevin@digitalpatrioten.com>
2013-09-17 11:01:55 +0200

    Changed JS

--- www/OCRPlugin.js
+++ www/OCRPlugin.js
@@ -1,6 +1,6 @@
-var OCRPlugin = { 
-	callNativeFunction: function (success, fail, resultType) { 
- 
-		return Cordova.exec( success, fail, "com.jcesarmobile.OCRPlugin", "recogniseOCR", [resultType]); 
-		} 
-	};
+var exec = require("cordova/exec");
+var OCRPlugin = {
+    callNativeFunction: function (success, fail, resultType) {
+        return exec(success, fail, "com.jcesarmobile.OCRPlugin", "recogniseOCR", [resultType]);
+    }
+};
